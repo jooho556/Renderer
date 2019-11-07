4781e27f02ab933505fa3a62e904add23492a2fd
jooho.jeong <jooho556@gmail.com>
2019-11-08 00:48:19 +0900

    Compute shader for black hole is added.
    Former-commit-id: d19deb60467bde17d5ad0f986f3ccb09b374caea

--- Renderer/Renderer/Shaders/Particle.cs
+++ Renderer/Renderer/Shaders/Particle.cs
@@ -1,18 +1,12 @@
 #version 440 core
+#define EPS2 0.2
 
 layout(local_size_x = 1000) in;
 
-uniform float Gravity1 = 100;
-uniform vec3 BlackHolePos1;
-
-uniform float Gravity2 = 100;
-uniform vec3 BlackHolePos2;
-
-uniform float ParticleMass = 1;
-uniform float ParticleInvMass = 1.0;
-
-uniform float dt = 0.0005;
-uniform float MaxDist = 10;
+uniform uint particle_num = 10000;
+uniform float dt = 0.00005f;
+uniform float damping = 0.9999;
+uniform float G = 2.f;
 
 layout(std430, binding = 0) buffer Pos
 {
@@ -24,31 +18,56 @@ layout(std430, binding = 1) buffer Vel
     vec4 Velocity [];
 };
 
+layout(std430, binding = 2) buffer M
+{
+    float Mass [];
+};
+
+vec3 GetForce(in vec4 p1, in vec4 p2)
+{
+    vec3 radius = p2.xyz - p1.xyz;
+    float r_sqr = dot(radius, radius) + EPS2;
+    float r_sixth = r_sqr * r_sqr * r_sqr;
+    float inv_dist_cube = inversesqrt(r_sixth);
+    return radius * inv_dist_cube;
+}
+
 void main()
 {
     uint id = gl_GlobalInvocationID.x;
+    vec4 pos = Position[id];
+    vec3 net_force = vec3(0, 0, 0);
 
-    vec3 pos = Position[id].xyz;
-
-    //Force from bh1
-    vec3 d = BlackHolePos1 - pos;
-    float dist = length(d);
-    vec3 force = (Gravity1 / dist) * normalize(d);
-
-    //Force from bh2
-    d = BlackHolePos2 - pos;
-    dist = length(d);
-    force += (Gravity2 / dist) * normalize(d);
-
-    if(dist > MaxDist)
+    for (uint i = 0; i < particle_num; ++i)
     {
-        Position[id] = vec4(0, 0, 0, 1);
-    }
-    else
-    {
-        vec3 accel = force * ParticleInvMass;
-        Position[id] = vec4(pos + Velocity[id].xyz * dt + 0.5 * accel * dt * dt, 1.f);
-        Velocity[id] = vec4(Velocity[id].xyz + accel * dt, 0);
+        vec4 particle = Position[i];
+
+        vec3 force = GetForce(pos, particle);
+
+        if (i != id)
+            net_force += force;
     }
 
-}
+    vec3 final_vel = dt * net_force * G + Velocity[id].xyz * damping;
+    Velocity[id] = vec4(final_vel, 0.f);
+    Position[id] = vec4(pos.xyz + dt * Velocity[id].xyz, 1.f);
+
+    //vec3 net_force = vec3(0, 0, 0);
+
+    //for(uint i = 0; i < particle_num; ++i)
+    //{
+    //    float dist = distance(Position[i], Position[id]);
+    //    float g = 1.0 / dist * 0.1;
+
+    //    vec3 dir = Position[i].xyz - Position[id].xyz;
+    //    dir = dir / (length(dir) + 1e-10);
+
+    //    vec3 force = g * dir * 0.001;
+
+    //    if (i != id)
+    //        net_force += force;
+    //}
+
+    //Position[id] += Velocity[id] + vec4(net_force / 2.f * 0.005f, 0.f);
+    //Velocity[id] += vec4(net_force * 0.005f, 0.f);
+} 
