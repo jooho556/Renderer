0c57022ede2a30adf6f335b96a44ffd9b86b8c76
jooho.jeong <jooho556@gmail.com>
2019-12-16 01:51:30 +0900

    Improper output fixed in Nebula.fs.
    TODO: Applying fractal sum
    
    Former-commit-id: 9e700a8cc5b57424674a9423d1ed985e458103df [formerly 6ba0aea3568a043f4cf0cf1ca49d8d4790e7a899]
    Former-commit-id: 4c0f7c6c876e6cdaa6bea000505820e18aca600e

--- Renderer/Renderer/Shaders/Nebula.fs
+++ Renderer/Renderer/Shaders/Nebula.fs
@@ -15,12 +15,12 @@ uint Hash(uint x, uint y)
     return permutations[permutations[x] + y];
 }
 
-vec2 GetPoint(float x, float y) // fragcoord.x, fragcoord.y
+vec2 GetPoint(float x, float y, int t_size) // fragcoord.x, fragcoord.y
 {
-    uint unit_x_length = uint(screen_width / table_size); // 1024x1024, table_size = 256, then width of one grid is 4.
-    uint unit_y_length = uint(screen_height / table_size);
+    uint unit_x_length = uint(screen_width / t_size); // 1024x1024, table_size = 256, then width of one grid is 4.
+    uint unit_y_length = uint(screen_height / t_size);
 
-    return vec2(float(x / unit_x_length), float(y / unit_y_length));
+    return vec2(float(x / float(unit_x_length)), float(y / float(unit_y_length)));
 }
 
 float lerp(float a, float b, float t)
@@ -28,22 +28,22 @@ float lerp(float a, float b, float t)
     return a + t * (b - a);
 }
 
-float GetPerlinNoise(vec2 p)
+float GetPerlinNoise(vec2 p, int t_size)
 {
-    int table_size_mask = table_size - 1;
+    int table_size_mask = t_size - 1;
 
     //Four corners
     int xi0 = (int(floor(p.x))) & table_size_mask;
     int yi0 = (int(floor(p.y))) & table_size_mask;
     int xi1 = (xi0 + 1) & table_size_mask;
-    int yi1 = (xi0 + 1) & table_size_mask;
+    int yi1 = (yi0 + 1) & table_size_mask;
 
     //Fractional part in local grid
     float tx = p.x - (int(floor(p.x)));
     float ty = p.y - (int(floor(p.y)));
 
     float t1 = smoothstep(0.f, 1.f, tx);
-    float t2 = smoothstep(0.f, 1.f, tx);
+    float t2 = smoothstep(0.f, 1.f, ty);
 
     //Getting gradient of each corner
     vec2 g_bot_left = gradients[Hash(xi0, yi0)];
@@ -64,21 +64,41 @@ float GetPerlinNoise(vec2 p)
 
     //Dot product
     float bot_left_val = dot(g_bot_left, bot_left_to_p);
+    bot_left_val = (bot_left_val + 1) / 2.f;
     float bot_right_val = dot(g_bot_right, bot_right_to_p);
+    bot_right_val = (bot_right_val + 1) / 2.f;
     float top_right_val = dot(g_top_right, top_right_to_p);
+    top_right_val = (top_right_val + 1) / 2.f;
     float top_left_val = dot(g_top_left, top_left_to_p);
+    top_left_val = (top_left_val + 1) / 2.f;
 
     //Bi-linear interpolation
-    float bottom_edge_x = mix(bot_left_val, bot_right_val, t1);
-    float top_edge_x = mix(top_left_val, top_right_val, t1);
-    float final_result = mix(bottom_edge_x, top_edge_x, t2);
+    float bottom_edge_x = lerp(bot_left_val, bot_right_val, t1);
+    float top_edge_x = lerp(top_left_val, top_right_val, t1);
+    float final_result = lerp(bottom_edge_x, top_edge_x, t2);
 
-    return (final_result + 1.f) / 2.f;
+    return final_result;
+}
+
+float Turbulence(vec2 p, int t_size)
+{
+    float value = 0.f;
+
+    float i = 0.f;
+    while(t_size <= int(table_size))
+    {
+        value += GetPerlinNoise(p, t_size);
+        t_size *= 2;
+        i++;
+    }
+
+    return value / i;
 }
 
 void main()
 {
     vec3 s = texture2D(stars, tex_coord).rgb;
-    float n = GetPerlinNoise(GetPoint(gl_FragCoord.x, gl_FragCoord.y));
+    //float n = GetPerlinNoise(GetPoint(gl_FragCoord.x, gl_FragCoord.y, 4), 4);
+    float n = Turbulence(GetPoint(gl_FragCoord.x, gl_FragCoord.y, 8), 8);
     gl_FragColor = vec4(mix(s, vec3(0.4f, 0.1f, 0.4f), n), 1.f);
 }
