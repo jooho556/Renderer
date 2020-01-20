9eff23bbd6d8cfbd00df4886ed65bbe4e82e198b
jooho.jeong <jooho556@gmail.com>
2020-01-20 16:48:28 +0900

    3D perlin noise algorithm is implemented
    Former-commit-id: a29b77440cdcfe1e377781c3d4f0b3b58b020a56

--- Renderer/Renderer/PerlinNoise.h
+++ Renderer/Renderer/PerlinNoise.h
@@ -19,3 +19,22 @@ private:
     std::vector<glm::vec2> gradients;
     std::vector<unsigned int> permutation_table;
 };
+
+class PerlinNoise3D
+{
+public:
+    PerlinNoise3D();
+    std::vector<float> GetNoiseData() const;
+    void GenerateImages(unsigned int layer_num) const;
+
+private:
+    glm::vec3 GetPoint(float x, float y, float z) const;
+    unsigned int Hash(unsigned int x, unsigned int y, unsigned int z) const;
+    float GetPerlinNoise(const glm::vec3& point) const;
+    float FractalSum(glm::vec3 point, int layer_num, float& maximum_value) const;
+
+    const unsigned int table_size;
+    const unsigned int noise_side_length;
+    std::vector<glm::vec3> gradients;
+    std::vector<unsigned int> permutation_table;
+};
